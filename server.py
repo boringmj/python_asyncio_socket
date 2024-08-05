e0500e5eb7361d520a8af177537aa8ff288a0ea9
boringmj <2878663643@qq.com>
2024-08-06 04:19:01 +0800

    删除多余示例代码.让demo看起来更加简洁

--- server.py
+++ server.py
@@ -7,23 +7,6 @@ class MyServer(Server):
         print(f'来自 {addr} 的数据:{data}')
         await connect.send(data)
         await connect.close()
-    
-    async def _reject_client(self,connect:Connect)->None:
-        # 这里可以重写拒绝连接处理
-        print(f'拒绝来自 {connect.peername()} 的连接')
-        return await super()._reject_client(connect)
-    
-    async def _queue_error(self,connect:Connect,error:Exception)->None:
-        # 这里可以重写排队中的连接错误处理
-        return await super()._queue_error(connect,error)
-
-    async def _error(self,addr,error:Exception)->None:
-        # 这里可以重写连接错误处理
-        return await super()._error(addr,error)
-
-    def _server_error(self,error:Exception)->None:
-        # 这里可以重写服务器错误处理
-        return super()._server_error(error)
 
 # 服务端
 MyServer(listen_keywords=True)
