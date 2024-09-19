9b9bac06489406d3c139820c843088ab1f4c819a
SunYian <winksunyian@gmail.com>
2024-09-19 14:56:18 +0800

    charroom

--- client.py
+++ client.py
@@ -1,45 +1,40 @@
-from tcp_quick.client import Client,Connect
+from tcp_quick.client import Client, Connect
 import traceback
-# 如果你想要使用ssl,请取消下面的注释
-import ssl
+import asyncio
+
 
 class MyClient(Client):
-    async def _handle(self,connect:Connect)->None:
-        message='Hi,Server!'
-        await connect.send(message.encode(),timeout=120)
-        data=await connect.recv(120) # 注意,这里的120是指定的超时时间不是读取的大小,如果超时将会抛出异常
-        print(f'接收数据:{data.decode()}')
-        # 接收一个原始的数据包
-        data=await connect.recv_raw(1024,120) # 第一个参数是指定的读取大小,第二个参数是指定的超时时间
-        print(f'接收原始数据:{data.decode()}')
-        await connect.close()
+    def __init__(self, user_name="", password="", **kwargs):
+        self.user_name = user_name
+        self.password = password
+        super().__init__(**kwargs)
 
-    async def _error(self,e:Exception)->None:
+    async def _handle(self, connect: Connect) -> None:
+        """处理连接"""
+        await connect.send(self.user_name.encode())
+        await connect.send(self.password.encode())
+        # 使用 asyncio.create_task 来异步运行输入函数
+        self._loop.create_task(self._listen_keyboard_input())
+        while True:
+            data = await connect.recv()
+            print(f"{data.decode()}")
+
+    async def _listen_keyboard_input(self) -> str:
+        """非阻塞输入函数"""
+        while True:
+            loop = asyncio.get_running_loop()
+            future = loop.run_in_executor(None, input)
+            await self.send((await future).encode())
+
+    async def _error(self, e: Exception) -> None:
+        pass
         """处理错误"""
-        print(f'发生错误:{e}') 
+        print(f"发生错误:{e}")
         # 如果你想要更详细的错误信息,可以使用traceback模块
-        traceback_details=''.join(traceback.format_exception(type(e),e,e.__traceback__))
+        traceback_details = "".join(
+            traceback.format_exception(type(e), e, e.__traceback__)
+        )
         print(traceback_details)
 
-# 客户端
-# 请注意,行模式(use_line)并不适合传输超过缓冲区大小的数据,如果在缓冲区没有读取到换行符,将会抛出异常
-# 行模式使用的是StreamReader的readline方法
-# 经过测试抛出的异常为`valueError: Separator is not found, and chunk exceed the limit`
-# 被Connect捕获后为`ValueError: 行数据异常: Separator is not found, and chunk exceed the limit`
-# 请注意,如果你的服务端使用了行模式,客户端也需要使用行模式,同理,如果服务端没有使用行模式,客户端也不需要使用行模式
-# 客户端配置大部分情况下需要与服务端配置保持一致,未来可能会考虑自动配置(目前不支持)
 
-# 这是一个简单的客户端实例
-# MyClient(use_line=True,use_aes=False)
-
-# 演示使用ssl
-ssl_context=ssl.create_default_context()
-# 设置具有前向保密的密码套件
-ssl_context.set_ciphers(
-    'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:'
-    'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256'
-)
-# 跳过证书验证和主机名验证,不建议在生产环境中使用
-ssl_context.check_hostname=False
-ssl_context.verify_mode=ssl.CERT_NONE
-client=MyClient(ssl=ssl_context,use_line=True)
+client = MyClient(user_name="abc", password="123", use_line=False, use_aes=False)
--- server.py
+++ server.py
@@ -1,78 +1,66 @@
-from tcp_quick.server import Server,Connect
+from tcp_quick.server import Server, Connect
 import traceback
+
 # 如果你想要使用ssl,请取消下面的注释
 from tcp_quick.cert_manager import CertManager
-import os,ssl
+import os, ssl
+
+
+user = {"abc": "123", "kkk": "111"}
+
 
 class MyServer(Server):
-    async def _handle(self,connect:Connect)->None:
-        addr=connect.peername()
-        print(f'连接: {addr} 已建立')
-        data=await connect.recv(120) # 注意,这里的120是指定的超时时间不是读取的大小,如果超时将会抛出异常
-        print(f'来自 {addr} 的数据:{data.decode()}')
-        message='Hello,Client!'
-        await connect.send(message.encode(),timeout=120)
-        # 发送一个原始的数据包
-        message='This is a raw data packet.'
-        await connect.send_raw(message.encode(),timeout=120)
-        await connect.close()
 
-    async def _error(self,addr,e:Exception)->None:
-        print(f'来自 {addr} 的连接出现错误: {e}')
+    online_users: dict[str] = {}
+
+    async def _handle(self, connect: Connect) -> None:
+        addr = connect.peername()
+        print(f"{addr}:已连接")
+        user_name = (await connect.recv(60)).decode()
+        password = (await connect.recv(60)).decode()
+        if not await self.login(user_name, password):
+            await self.send_utf(connect, "用户名或密码错误")
+            raise Exception("登录失败")
+        await self.send_utf(connect, "登录成功")
+
+        self.online_users[user_name] = connect
+        # 循环接收客户端数据
+        while True:
+            data = await connect.recv()
+            full_data = data.decode()
+            print(f"{addr}: {full_data}")
+            if full_data.startswith("@"):
+                target, msg = full_data[1:].split(" ", 1)
+                if target not in self.online_users:
+                    await self.send_utf(connect, "用户不存在")
+                await self.send_utf(self.online_users[target], f"{user_name}:{msg}")
+
+    async def _error(self, addr, e: Exception) -> None:
+        pass
+        print(f"来自 {addr} 的连接出现错误: {e}")
         # 如果你想要更详细的错误信息,可以使用traceback模块
-        traceback_details=''.join(traceback.format_exception(type(e),e,e.__traceback__))
+        traceback_details = "".join(
+            traceback.format_exception(type(e), e, e.__traceback__)
+        )
         print(traceback_details)
 
-    async def _connection_closed(self,addr,connect:Connect)->None:
-        # 请在这里重写连接成功的连接被关闭时的处理(无论是正常关闭还是异常关闭),如果不重写,可以删除这个方法
-        await super()._connection_closed(addr,connect)
+    async def _connection_closed(self, addr: tuple[str, int], connect: Connect) -> None:
+        for u in self.online_users:
+            if self.online_users[u].peername() == addr:
+                del self.online_users[u]
+                break
+        await self.close(connect)
 
-# 服务端
-# 请注意,行模式(use_line)并不适合传输超过缓冲区大小的数据,如果在缓冲区没有读取到换行符,将会抛出异常
-# 行模式使用的是StreamReader的readline方法
-# 经过测试抛出的异常为`valueError: Separator is not found, and chunk exceed the limit`
-# 被Connect捕获后为`ValueError: 行数据异常: Separator is not found, and chunk exceed the limit`
-# 是否使用aes加密(use_aes)默认情况下为自动选择,即启用ssl时自动关闭aes加密,关闭ssl时自动开启aes加密
-# 开启aes加密时,服务器会与客户端进行简单的密钥交换,密钥交换使用的是RSA算法,RSA公钥需要客户端手动确认是否信任
-# 如果有需求可以重写相关方法,他们的逻辑在Connect类中,但实际上你可以直接在Server类中重写秘钥交换的方法
-# 如果不需要请手动关闭(这里更加推荐使用ssl)
+    async def send_utf(self, connect: Connect, msg: str | bytes, **args):
+        if type(msg) == str:
+            msg = msg.encode()
+        await self.send(connect, msg, **args)
 
-# 这是一个简单的服务端实例
-# MyServer(listen_keywords=True,use_line=True,use_aes=False)
+    async def login(self, user_name: str, password: str) -> bool:
+        if user_name in user and user[user_name] == password:
+            return True
+        else:
+            return False
 
-# 演示使用ssl
-private_key_path='test/private.key'
-certificate_path='test/certificate.crt'
-ssl_context=ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-# 判断test目录是否存在
-if not os.path.exists('test'):
-    os.mkdir('test')
-# 判断证书和私钥是否同时存在
-if not os.path.exists(private_key_path) or not os.path.exists(certificate_path):
-    # 生成证书和私钥
-    private_key=CertManager.generate_private_key()
-    certificate=CertManager.generate_certificate(
-        private_key,
-        CertManager.build_x509_name(common_name='localhost'),
-        CertManager.build_x509_name(common_name='localhost'),
-        valid_days=365,
-        output_private_key_path=private_key_path,
-        output_certificate_path=certificate_path
-    )
-# 校验证书是否有效
-certificate=CertManager.load_certificate_from_pem_file(certificate_path)
-if not CertManager.check_certificate_validity(certificate):
-    raise ValueError('证书已过期')
-# 加载私钥
-private_key=CertManager.load_private_key_from_pem_file(private_key_path)
-# 校验证书和私钥是否匹配
-if not CertManager.check_certificate_private_key_match(certificate,private_key):
-    raise ValueError('证书和私钥不匹配')
-# 加载证书
-ssl_context.load_cert_chain(certificate_path,private_key_path)
-# 如果你有CA证书,可以使用下面的方法加载CA证书
-# ssl_context.load_verify_locations(cafile='this_is_ca.crt')
-server=MyServer(listen_keywords=True,ssl=ssl_context,use_line=True)
 
-# 更多参数请参考Server类的__init__方法
-# server=MyServer(host='0.0.0.0',port=12345,backlog=1,reject=False,listen_keywords=True,use_line=False)
+server = MyServer(listen_keywords=True, use_line=False, use_aes=False)
